81b4f7abeb1eb73707fc1675b819a0ca3b87d2a8
Sebekerga <sebekerka@gmail.com>
2017-04-14 00:09:31 +0700

    очень лень писать чо-то работает я спать

--- app/src/main/java/com/sebekerga/tjee4/MainActivity.java
+++ app/src/main/java/com/sebekerga/tjee4/MainActivity.java
@@ -1,13 +1,96 @@
 package com.sebekerga.tjee4;
 
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final int duration = 1; // seconds
+    private final int sampleRate = 8000;
+    private final int numSamples = duration * sampleRate;
+    private final double sample[] = new double[numSamples];
+    private double freqOfTone = 4400; // hz
+
+    private final byte generatedSnd[] = new byte[2 * numSamples];
+
+    Handler handler = new Handler();
+
+    String data;
+
+    EditText edit_text_data;
+    EditText edit_text_fr_zero;
+    EditText edit_text_fr_one;
+    Button button_send;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edit_text_data = (EditText) findViewById(R.id.edit_text_data);
+        edit_text_fr_zero = (EditText) findViewById(R.id.edit_text_fr_zero);
+        edit_text_fr_one = (EditText) findViewById(R.id.edit_text_fr_one);
+        button_send = (Button) findViewById(R.id.button_send);
+
+        button_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                data = edit_text_data.getText().toString();
+                for (char i : data.toCharArray()) {
+                    switch (i) {
+                        case '1':
+                            freqOfTone = 4400;
+                            break;
+                        case '0':
+                            freqOfTone = 440;
+                            break;
+
+                    }
+                    genTone();
+                    playSound();
+                    try {
+                        Thread.sleep(duration * 1000);
+                    } catch (InterruptedException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+
+    void genTone() {
+        // fill out the array
+        for (int i = 0; i < numSamples; ++i) {
+            sample[i] = Math.sin(2 * Math.PI * i / (sampleRate / freqOfTone));
+        }
+
+        // convert to 16 bit pcm sound array
+        // assumes the sample buffer is normalised.
+        int idx = 0;
+        for (final double dVal : sample) {
+            // scale to maximum amplitude
+            final short val = (short) ((dVal * 32767));
+            // in 16 bit wav PCM, first byte is the low order byte
+            generatedSnd[idx++] = (byte) (val & 0x00ff);
+            generatedSnd[idx++] = (byte) ((val & 0xff00) >>> 8);
+
+        }
+    }
+
+    void playSound() {
+        final AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
+                sampleRate, AudioFormat.CHANNEL_CONFIGURATION_MONO,
+                AudioFormat.ENCODING_PCM_16BIT, numSamples,
+                AudioTrack.MODE_STATIC);
+        audioTrack.write(generatedSnd, 0, generatedSnd.length);
+        audioTrack.play();
     }
 }
