877763c7a94c5e4413080ae43a66de166ce3608f
Sebekerga <sebekerka@gmail.com>
2017-04-18 23:28:32 +0700

    что-то работает... наверное должно

--- app/src/main/java/com/sebekerga/tjee4/MainReceiveActivity.java
+++ app/src/main/java/com/sebekerga/tjee4/MainReceiveActivity.java
@@ -1,13 +1,78 @@
 package com.sebekerga.tjee4;
 
-import android.support.v7.app.AppCompatActivity;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.renderscript.Sampler;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainReceiveActivity extends AppCompatActivity {
+
+    private static final int RECORDER_SAMPLERATE = 44100;
+    private static final int RECORDER_CHANNELS = AudioFormat.CHANNEL_IN_MONO;
+    private static final int RECORDER_AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT;
+    private AudioRecord recorder = null;
+    private Thread recordingThread = null;
+    private boolean isRecording = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_recieve);
+
+        int bufferSize = AudioRecord.getMinBufferSize(RECORDER_SAMPLERATE,
+                RECORDER_CHANNELS, RECORDER_AUDIO_ENCODING);
+
+        recorder = new AudioRecord(MediaRecorder.AudioSource.MIC,
+                RECORDER_SAMPLERATE, RECORDER_CHANNELS,
+                RECORDER_AUDIO_ENCODING, BufferElements2Rec * BytesPerElement);
+
+        recorder.startRecording();
+        isRecording = true;
+        recordingThread = new Thread(new Runnable() {
+            public void run() {
+                scanForBits();
+            }
+        }, "AudioRecorder Thread");
+        recordingThread.start();
+
+
+    }
+
+    int BufferElements2Rec = 1024; // want to play 2048 (2K) since 2 bytes we use only 1024
+    int BytesPerElement = 2; // 2 bytes in 16bit format
+
+    private void scanForBits(){
+
+        short sData[] = new short[BufferElements2Rec];
+        while (isRecording) {
+            // gets the voice output from microphone to byte format
+
+            recorder.read(sData, 0, BufferElements2Rec);
+            System.out.println("Short wirting to file" + sData.toString());
+            Log.i("FR", Integer.toString(calculate(RECORDER_SAMPLERATE, sData)));
+        }
+
+    }
+
+    public static int calculate(int sampleRate, short [] audioData){
+
+        int numSamples = audioData.length;
+        int numCrossing = 0;
+        for (int p = 0; p < numSamples-1; p++)
+        {
+            if ((audioData[p] > 0 && audioData[p + 1] <= 0) ||
+                    (audioData[p] < 0 && audioData[p + 1] >= 0))
+            {
+                numCrossing++;
+            }
+        }
+        float numSecondsRecorded = (float)numSamples/(float)sampleRate;
+        float numCycles = numCrossing/2;
+        float frequency = numCycles/numSecondsRecorded;
+
+        return (int)frequency;
     }
 }
--- app/src/main/java/com/sebekerga/tjee4/MainSendActivity.java
+++ app/src/main/java/com/sebekerga/tjee4/MainSendActivity.java
@@ -32,7 +32,7 @@ public class MainSendActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_send);
 
         edit_text_data = (EditText) findViewById(R.id.edit_text_data);
         edit_text_fr_zero = (EditText) findViewById(R.id.edit_text_fr_zero);
