737cf0ef45a1fe558c8e98ae48e60718ec5ce238
Sebekerga <sebekerka@gmail.com>
2017-04-19 12:27:07 +0700

    маАагия

--- app/src/main/java/com/sebekerga/tjee4/ChooseRoleActivity.java
+++ app/src/main/java/com/sebekerga/tjee4/ChooseRoleActivity.java
@@ -1,7 +1,10 @@
 package com.sebekerga.tjee4;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ChooseRoleActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class ChooseRoleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_role);
+
+        Button button_send = (Button) findViewById(R.id.button_send);
+        Button button_receive = (Button) findViewById(R.id.button_receive);
+
+        button_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ChooseRoleActivity.this, MainSendActivity.class));
+            }
+        });
+
+        button_receive.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ChooseRoleActivity.this, MainReceiveActivity.class));
+            }
+        });
     }
 }
